473aceff2f172fe83d1efd2321fba313a62d6b03
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-22 12:12:00 +0100

    returns inital temp of 20 degrees

---
+++ thermostat.js
@@ -0,0 +1,17 @@
+class Thermostat {
+  // constructor() {
+  //   this.temp = 20;
+   
+  // }
+
+  temp() {
+
+    return 20;
+  }
+
+  // up(amount) {
+  //   this.temp += amount;
+  // }
+}
+
+module.exports = Thermostat;
---
+++ thermostat.test.js
@@ -0,0 +1,14 @@
+const Thermostat = require('./thermostat');
+
+describe(Thermostat, () => {
+  it('starts with an initial temp of 20 degrees', () => {
+    const thermostat = new Thermostat();
+    expect(thermostat.temp()).toBe(20);
+  })
+
+  // it('you can increase the temperature with an up method', () => {
+  //   const thermostat = new Thermostat();
+  //   thermostat.up(10);
+  //   expect(thermostat.temp()).toEqual(30)
+  // })
+})
